4882edb4f96ae3a110ea5d46b7aa48e2b3419535
Bartosz Sech <trilinemail@gmail.com>
2024-06-28 13:11:07 +0200

    Delete tasks from TODO list

--- js/todo.js
+++ js/todo.js
@@ -1,7 +1,17 @@
 
+var counter = 0
+
 document.addEventListener("keyup", function(event) {
     if (event.key === "Enter" && document.getElementById("input").value != "") {
         var input = document.getElementById("input").value;
-        document.getElementById("todo-list").innerHTML += "<li>" + input + "</li>";
+        document.getElementById("todo-list").innerHTML += "<li id=\"counter" + counter + "\">" + 
+        "<button onclick=\"deleteTask(" + counter + ")\"> x </button> " + 
+        "<span>" + input + "<span>" + "</li>";
+        counter++;
     }
 });
+
+function deleteTask(TaskId){
+    var target = document.getElementById("counter" + TaskId);
+    target.remove();
+}
